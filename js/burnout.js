9e2d04c47bc32631f9fc0a14c280ff9adee78020
HealYourHeart-App <cxnninphdlzxcv@gmail.com>
2026-08-06 11:04:35 +0700

    Update burnout.js

--- js/burnout.js
+++ js/burnout.js
@@ -2,10 +2,10 @@ document.getElementById('burnoutForm').addEventListener('submit', function(e) {
     e.preventDefault();
     
     // ดึงค่าที่ผู้ใช้เลือก (บังคับเลือกครบด้วย required แล้ว)
-    const b0 = parseInt(document.querySelector('input[name="b1"]:checked').value);
-    const b1 = parseInt(document.querySelector('input[name="b2"]:checked').value);
-    const b2 = parseInt(document.querySelector('input[name="b3"]:checked').value);
-    const b3 = parseInt(document.querySelector('input[name="b1"]:checked').value);
+    const b0 = parseInt(document.querySelector('input[name="b0"]:checked').value);
+    const b1 = parseInt(document.querySelector('input[name="b1"]:checked').value);
+    const b2 = parseInt(document.querySelector('input[name="b2"]:checked').value);
+    const b3 = parseInt(document.querySelector('input[name="b3"]:checked').value);
     // รวมคะแนน
     const totalScore = b0 + b1 + b2 + b3;
 
